6ca37158e520efb970001df0eff75f4d696e3630
Tanmoy's- Pc <tonmoytonu55@gmail.com>
2023-11-04 21:14:42 +0600

    commit

---
+++ app/src/main/java/com/example/biyashadi/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.biyashadi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
